e74ae4f1036d6596b3a59badebee08455f26b132
milan3991 <m.mircic93@gmail.com>
2025-04-15 13:16:27 +0200

    worksupdate

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -2,7 +2,9 @@ import Heading from '../../components/Heading/Heading';
 import './contact.style.css'
 
 const Contact = () => {
+    return(
     <Heading heading="CONTACT ME" headingbackground="CONTACT"/>
+    )
 }
 
 export default Contact;
