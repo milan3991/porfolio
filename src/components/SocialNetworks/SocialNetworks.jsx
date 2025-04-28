d1b19a034e02726ef6dc576fa9f676909b48706b
milan3991 <m.mircic93@gmail.com>
2025-04-28 08:14:21 +0200

    socialnetworksupdate

--- src/components/SocialNetworks/SocialNetworks.jsx
+++ src/components/SocialNetworks/SocialNetworks.jsx
@@ -1,13 +1,18 @@
 import '../SocialNetworks/socialnetwork.style.css'
+import facebook from '../../assets/socialnetworkicons/facebook.svg';
+import github from '../../assets/socialnetworkicons/github.svg';
+import twitter from '../../assets/socialnetworkicons/twitter.svg';
+import website from '../../assets/socialnetworkicons/website.svg';
+import linkedin from '../../assets/socialnetworkicons/linkedin.svg';
 
 const SocialNetwork = ( { facebookUrl,twitterUrl, githubUrl, websiteUrl, linkedinUrl} ) => {
     return (
             <div className='social_networks'>
-                <a href={facebookUrl} target='_blank'><img src='src\assets\socialnetworkicons\facebook.svg'/></a>
-                <a href={githubUrl} target='_blank'><img src='src\assets\socialnetworkicons\github.svg'/></a>
-                <a href={twitterUrl} target='_blank'><img src='src\assets\socialnetworkicons\twitter.svg'/></a>
-                <a href={websiteUrl} target='_blank'><img src='src\assets\socialnetworkicons\website.svg'/></a>
-                <a href={linkedinUrl} target='_blank'><img src='src\assets\socialnetworkicons\linkedin.svg'/></a>
+                <a href={facebookUrl} target='_blank'><img src={facebook}/></a>
+                <a href={githubUrl} target='_blank'><img src={github}/></a>
+                <a href={twitterUrl} target='_blank'><img src={twitter}/></a>
+                <a href={websiteUrl} target='_blank'><img src={website}/></a>
+                <a href={linkedinUrl} target='_blank'><img src={linkedin}/></a>
             </div>
     )
 };
